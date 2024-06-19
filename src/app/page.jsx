ec2753e0696a8662c47029dd4196a200d7c62f1f
sumitskj <sumitjangirdss.1@gmail.com>
2024-06-20 01:16:11 +0530

    search box

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,7 @@ import LoadingPageSkeleton from "@/components/LoadingPageSkeleton";
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     initPage();
@@ -15,7 +16,7 @@ const HomePage = () => {
   const getPosts = async (page, limit) => {
     try {
       const res = await fetchBackendApiWrapper(
-        `/post/public?limit=${limit}&offset=${page}`,
+        `/post/public?limit=${limit}&offset=${page}&searchQuery=${search}`,
         {
           method: "GET",
           cache: "no-store",
@@ -33,6 +34,16 @@ const HomePage = () => {
     }
   };
 
+  const handleSearch = () => {
+    initPage();
+  };
+
+  const handleSearchOnKeydown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  }
+
   const initPage = async () => {
     setLoading(true);
     const data = await getPosts(0, 4);
@@ -42,14 +53,32 @@ const HomePage = () => {
 
   return (
     <div className="rounded-lg w-full h-[86vh] overflow-y-auto">
-      {!loading && (
-        <ScrollingPagination initialItems={posts} getData={getPosts} />
-      )}
-      {loading && (
-        <div className="w-full flex justify-center items-center bg-white h-dvh">
-          <LoadingPageSkeleton />
+      <div className="flex flex-col justify-start items-start w-full h-full">
+        <div className="py-2 px-2 w-full flex justify-between items-center gap-4">
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleSearchOnKeydown}
+            placeholder="Search Post..."
+            className="w-full bg-white rounded-full border border-slate-200 px-4 py-3 focus:outline-none"
+          />
+          <button
+            onClick={handleSearch}
+            className="p-2 rounded-2xl bg-red-500 text-white"
+          >
+            Search
+          </button>
         </div>
-      )}
+        {!loading && (
+          <ScrollingPagination initialItems={posts} getData={getPosts} />
+        )}
+        {loading && (
+          <div className="w-full flex justify-center items-center h-dvh">
+            <LoadingPageSkeleton />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
